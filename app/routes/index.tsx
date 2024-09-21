95c6b917893ff01f83ffdbab2f62c0648f3589fd
kelvin27315 <development@none.cloud>
2024-09-22 01:14:11 +0900

    lint対応

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,14 +4,12 @@ import OldProvinceQuiz from "../islands/oldProvinceQuiz";
 
 export default createRoute((c) => {
   return c.render(
-    <>
-      <div>
-        <h1>旧国名読みテスト</h1>
-        <p>
-          次の漢字の読みをひらがなで記入しなさい。（全{oldProvinces.length}問）
-        </p>
-        <OldProvinceQuiz />
-      </div>
-    </>,
+    <div>
+      <h1>旧国名読みテスト</h1>
+      <p>
+        次の漢字の読みをひらがなで記入しなさい。（全{oldProvinces.length}問）
+      </p>
+      <OldProvinceQuiz />
+    </div>,
   );
 });
